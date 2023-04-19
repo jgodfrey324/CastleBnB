f4e9ff5f70c26354d855c0cb19d99e9b12690394
Jenna <jennagodfrey324@gmail.com>
2023-04-19 10:06:40 -0700

    create/seed bookings tables

--- authenticate-me/backend/db/models/spot.js
+++ authenticate-me/backend/db/models/spot.js
@@ -22,6 +22,9 @@ module.exports = (sequelize, DataTypes) => {
       Spot.hasMany(models.SpotImage, {
         foreignKey: 'spotId'
       });
+      Spot.hasMany(models.Booking, {
+        foreignKey: 'spotId'
+      });
     }
   }
   Spot.init({
