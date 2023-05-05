16b984b32534239afb7c02d75882a02c7619b0d5
Jenna <jennagodfrey324@gmail.com>
2023-05-05 16:59:31 -0700

    blocked buttons on login and signup form, added hidden effect to nav prof button

--- authenticate-me/frontend/src/components/Navigation/ProfileButton.js
+++ authenticate-me/frontend/src/components/Navigation/ProfileButton.js
@@ -4,18 +4,26 @@ import { logout } from '../../store/session';
 
 const ProfileButton = ({ user }) => {
   const dispatch = useDispatch();
+  const [showMenu, setShowMenu] = useState(false);
+
+//   const openMenu = () => {
+//     if (showMenu) return;
+//     setShowMenu(true);
+//   };
 
 //   const logout = (e) => {
 //     e.preventDefault();
 //     dispatch(logout());
 //   };
 
+  const ulClassName = "profile-dropdown" + (showMenu ? "" : " hidden");
+
   return (
     <>
-      <button className='profile-button' style={{color: '#0108B3'}}>
-      <i class="fa-regular fa-chess-queen fa-2xl"></i>
+      <button className='profile-button' style={{color: '#0108B3'}} onClick={() => setShowMenu(!showMenu)}>
+        <i class="fa-regular fa-chess-queen fa-l"></i>
       </button>
-      <ul className="profile-dropdown">
+      <ul className={ulClassName}>
         <li>{user.username}</li>
         <li>{user.firstName} {user.lastName}</li>
         <li>{user.email}</li>
