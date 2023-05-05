7a88d5d098627e2c5c124b7ef9a2c0473d968732
Jenna <jennagodfrey324@gmail.com>
2023-05-05 10:45:07 -0700

    signup user thunk func

--- authenticate-me/frontend/src/store/session.js
+++ authenticate-me/frontend/src/store/session.js
@@ -37,6 +37,23 @@ export const restoreUser = () => async (dispatch) => {
   return res;
 };
 
+export const signup = (user) => async (dispatch) => {
+  const { username, firstName, lastName, email, password } = user;
+  const res = await csrfFetch("/api/users", {
+    method: "POST",
+    body: JSON.stringify({
+      username,
+      firstName,
+      lastName,
+      email,
+      password,
+    }),
+  });
+  const data = await res.json();
+  dispatch(setUser(data.user));
+  return res;
+};
+
 //need logout
 
 const initialState = { user: null };
