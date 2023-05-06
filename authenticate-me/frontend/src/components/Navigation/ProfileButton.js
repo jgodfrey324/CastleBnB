a4619dccd51ac200b56fdc5f382463d2ed5729ee
Jenna <jennagodfrey324@gmail.com>
2023-05-06 14:44:48 -0700

    document event listener for drop down menu in ProfileButton Comp

--- authenticate-me/frontend/src/components/Navigation/ProfileButton.js
+++ authenticate-me/frontend/src/components/Navigation/ProfileButton.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { useDispatch } from 'react-redux';
 import { logout } from '../../store/session';
 
@@ -6,22 +6,38 @@ const ProfileButton = ({ user }) => {
   const dispatch = useDispatch();
   const [showMenu, setShowMenu] = useState(false);
 
-//   const openMenu = () => {
-//     if (showMenu) return;
-//     setShowMenu(true);
-//   };
+  //add event listener if showMenu is true, register to document, remove listener before running again
+  useEffect(() => {
+    //if showMenu false, we don't want any of this business (mostly on initial load)
+    if (!showMenu) return;
+
+    const closeMenu = (e) => {
+      setShowMenu(false);
+    };
+    document.addEventListener('click', closeMenu);
+
+    return () => document.removeEventListener("click", closeMenu);
+    //listen for when showMenu changes
+  }, [showMenu]);
+
+  //opens profile menu on button click
+  const openMenu = () => {
+    if (showMenu) return;
+    setShowMenu(true);
+  };
 
 //   const logout = (e) => {
 //     e.preventDefault();
 //     dispatch(logout());
 //   };
 
+  //sets class name for hidden menu functionality
   const ulClassName = "profile-dropdown" + (showMenu ? "" : " hidden");
 
   return (
     <>
-      <button className='profile-button' style={{color: '#0108B3'}} onClick={() => setShowMenu(!showMenu)}>
-        <i class="fa-regular fa-chess-queen fa-l"></i>
+      <button className='profile-button' style={{color: '#0108B3'}} onClick={openMenu}>
+        <i className="fa-regular fa-chess-queen fa-l"></i>
       </button>
       <ul className={ulClassName}>
         <li>{user.username}</li>
