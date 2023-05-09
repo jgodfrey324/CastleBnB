5864b122205e60132aec797c4cfb934b5adb2fb3
Jenna <jennagodfrey324@gmail.com>
2023-05-08 18:31:41 -0700

    added SpotImages comp to load spot images, css styling on SpotDetails

---
+++ authenticate-me/frontend/src/components/SpotDetails/SpotImages.js
@@ -0,0 +1,28 @@
+import { useSelector } from 'react-redux';
+
+const SpotImages = ({ spotId }) => {
+    const spot = useSelector(state => state.spots[spotId]);
+    const images = spot.SpotImages;
+    console.log('images: ', images);
+
+    if (!spot) return null;
+    if (!images.length) return <p>No current images</p>
+
+    const isPreview = (image) => {
+        if (image.preview) return 'preview';
+        return '';
+    }
+
+    return (
+        <div className='images-house'>
+            {images.map(image => {
+                return (
+                    <img key={spot.id} src={image.url} alt={`Gallery of ${spot.name}`} id={isPreview(image)}></img>
+                )
+            })}
+        </div>
+    )
+    // return <p>i'm the images, i'm here</p>;
+}
+
+export default SpotImages;
